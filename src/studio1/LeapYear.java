40ad29407ebdb9187e7e9dbd250f2585814f865b
Mara <Mara@DESKTOP-9HH0UB0.dhcp.wustl.edu>
2023-01-26 14:08:41 -0600

    hop like a bunny

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,15 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+	
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("What is the year?");
+	int year = in.nextInt();
+	boolean leap = (year%4 == 0) && (year%100 != 0);
+	System.out.println(year + " is a leap year: " +leap);
+	
+	}
+}

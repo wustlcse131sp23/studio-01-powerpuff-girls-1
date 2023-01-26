faf77b9ce37a1e0452b0a6790068ededecde0ede
Mara <Mara@DESKTOP-9HH0UB0.dhcp.wustl.edu>
2023-01-26 14:14:02 -0600

    now it hops even better

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
 	Scanner in = new Scanner(System.in);
 	System.out.println("What is the year?");
 	int year = in.nextInt();
-	boolean leap = (year%4 == 0) && (year%100 != 0);
+	boolean leap = (year%4 == 0) && (year%100 != 0) || (year%400 == 0);
 	System.out.println(year + " is a leap year: " +leap);
 	
 	}
